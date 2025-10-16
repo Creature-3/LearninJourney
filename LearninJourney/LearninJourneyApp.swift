358b4650d8703449dfed82b08440df532836f1b2
creature-3 <rahafalhammadi0@gmail.com>
2025-10-16 10:21:53 +0300

    Initial Commit

---
+++ LearninJourney/LearninJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearninJourneyApp.swift
+//  LearninJourney
+//
+//  Created by Rahaf Alhammadi on 24/04/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearninJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearninJourneyTests/LearninJourneyTests.swift
@@ -0,0 +1,16 @@
+//
+//  LearninJourneyTests.swift
+//  LearninJourneyTests
+//
+//  Created by Rahaf Alhammadi on 24/04/1447 AH.
+//
+
+import Testing
+
+struct LearninJourneyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
